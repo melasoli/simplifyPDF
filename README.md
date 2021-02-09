addbf0e795a5f14cdac0dcaadffd1a7a452d7e4a
melaniesoliven <49106525+melaniesoliven@users.noreply.github.com>
2021-02-08 19:17:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# simplifyPDF
+A simple tool to remove duplicate or near-duplicate PDF pages by comparing extracted text.
+
+Created for my uni notes!
---
+++ simplifyPDF.py
@@ -0,0 +1,42 @@
+'''
+A simple tool to remove duplicate or near-duplicate PDF pages by comparing extracted text.
+'''
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import fitz
+import easygui
+
+# File browser to open a file
+inFile = easygui.fileopenbox(msg='Open a PDF file', title='File Browser', filetypes=['*.pdf', 'PDF Files'])
+
+# File browser to save the file
+outFile = easygui.filesavebox(msg='Save the PDF file', title='File Browser', filetypes=["*.pdf", 'PDF Files'])
+
+# Keep track of pages to keep
+keepPages = []
+
+with open(inFile, 'rb') as f:
+    reader = PdfFileReader(f)
+    numOfPages = reader.getNumPages()
+
+    for pageNum in range(numOfPages-1):
+        # Get current page and next page
+        currPage = reader.getPage(pageNum)
+        nextPage = reader.getPage(pageNum+1)
+
+        # Extract Text from both current and next page
+        currText = currPage.extractText().strip()
+        nextText = nextPage.extractText().strip()
+
+        # Keep current page if next page doesn't already contain all text on current page
+        if (currText not in nextText):
+            keepPages.append(pageNum)
+
+    # Remember to add last page
+    keepPages.append(numOfPages-1)
+
+# Create a new file using the page numbers of pages to keep
+pdf = fitz.open(inFile)
+pdf.select(keepPages)
+pdf.save(outFile)
+pdf.close()
