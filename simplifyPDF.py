86a7a4469a62391a63205e849213042cf43a2060
melaniesoliven <49106525+melaniesoliven@users.noreply.github.com>
2021-02-15 22:56:24 -0500

    Fix #1

--- simplifyPDF.py
+++ simplifyPDF.py
@@ -5,6 +5,7 @@ A simple tool to remove duplicate or near-duplicate PDF pages by comparing extra
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import fitz
 import easygui
+import re
 
 # File browser to open a file
 inFile = easygui.fileopenbox(msg='Open a PDF file', title='File Browser', filetypes=['*.pdf', 'PDF Files'])
@@ -27,9 +28,13 @@ with open(inFile, 'rb') as f:
         # Extract Text from both current and next page
         currText = currPage.extractText().strip()
         nextText = nextPage.extractText().strip()
-
+       
         # Keep current page if next page doesn't already contain all text on current page
         if (currText not in nextText):
+            keepPages.append(pageNum)
+
+        # Sub-topic pages with titles that are used on the next page do not have page numbers written on the page
+        elif (re.search("\n[0-9][0-9]*$", currText, re.MULTILINE) == None):
             keepPages.append(pageNum)
 
     # Remember to add last page
