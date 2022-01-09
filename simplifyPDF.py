07a3b0ca9ab38c579d50a0e17a8731e0d848470b
melasoli <49106525+melasoli@users.noreply.github.com>
2022-01-09 00:37:21 -0500

    Replace EasyGUI file selection with Tkinter

--- simplifyPDF.py
+++ simplifyPDF.py
@@ -2,49 +2,65 @@
 A simple tool to remove duplicate or near-duplicate PDF pages by comparing extracted text.
 '''
 
-import fitz
-import easygui
+import fitz 
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 import re
 
-# File browser to open a file
-inFile = easygui.fileopenbox(msg='Open a PDF file', title='File Browser', filetypes=['*.pdf', 'PDF Files'])
+# Open main window
+window = tk.Tk()
+window.geometry('300x100')
+window.title('simplifyPDF')
 
-# File browser to save the file
-outFile = easygui.filesavebox(msg='Save the PDF file', title='File Browser', filetypes=["*.pdf", 'PDF Files'])
+# Open file, select pages that have only "complete" content, save file
+def keepPages():
 
-# Keep track of pages to keep
-keepPages = []
+    # File browser to open a file
+    inFile = askopenfilename(filetypes=[('PDF Files', '*.pdf')])
 
-# Open file
-f = fitz.open(inFile)
+    # File browser to save the file
+    outFile = asksaveasfilename(filetypes=[('PDF Files', '*.pdf')])
 
-numOfPages = f.page_count
+    # Keep track of pages to keep
+    toKeep = []
 
-for pageNum in range(numOfPages-1):
-    # Get current page and next page
-    currPage = f[pageNum]
-    nextPage = f[pageNum+1]
+    # Open file
+    f = fitz.open(inFile)
 
-    # Extract text from both current and next page
-    # Remove all whitespaces
-    currText = currPage.get_text("text").encode("ascii", "ignore").decode('ascii')
-    nextText = nextPage.get_text("text").encode("ascii", "ignore").decode('ascii')
+    numOfPages = f.page_count
 
-    currStrip = re.sub(r"(\s|[0-9]+$)", "", currText)
-    nextStrip = re.sub(r"(\s|[0-9]+$)", "", nextText)
+    for pageNum in range(numOfPages-1):
+        # Get current page and next page
+        currPage = f[pageNum]
+        nextPage = f[pageNum+1]
 
-    # Keep current page if next page doesn't already contain all text on current page
-    if (currStrip not in nextStrip):
-        keepPages.append(pageNum)
+        # Extract text from both current and next page
+        # Remove all whitespaces
+        currText = currPage.get_text("text").encode("ascii", "ignore").decode('ascii')
+        nextText = nextPage.get_text("text").encode("ascii", "ignore").decode('ascii')
 
-    # Sub-topic pages with titles that are used on the next page do not have page numbers written on the page
-    elif (re.search("\n[0-9][0-9]*$", currText, re.MULTILINE) == None):
-        keepPages.append(pageNum)
+        currStrip = re.sub(r"(\s|[0-9]+$)", "", currText)
+        nextStrip = re.sub(r"(\s|[0-9]+$)", "", nextText)
 
-# Remember to add last page
-keepPages.append(numOfPages-1)
+        # Keep current page if next page doesn't already contain all text on current page
+        if (currStrip not in nextStrip):
+            toKeep.append(pageNum)
 
-# Create a new file using the page numbers of pages to keep
-f.select(keepPages)
-f.save('{0}.pdf'.format(outFile))
-f.close()
+        # Sub-topic pages with titles that are used on the next page do not have page numbers written on the page
+        elif (re.search("\n[0-9][0-9]*$", currText, re.MULTILINE) == None):
+            toKeep.append(pageNum)
+
+    # Remember to add last page
+    toKeep.append(numOfPages-1)
+
+    # Create a new file using the page numbers of pages to keep
+    f.select(toKeep)
+    f.save('{0}.pdf'.format(outFile))
+    f.close()
+
+# Add button to open file browser
+btn = ttk.Button(window, text="Open PDF File", command=lambda : keepPages())
+btn.pack(side='top', pady=20)
+
+window.mainloop()
